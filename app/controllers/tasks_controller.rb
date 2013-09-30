1518552ae32ce6c8b88ae80e9212eddb32b25ca9
Sneha Keshwani <snehak@wharton.upenn.edu>
2013-09-30 16:02:09 -0400

    added conrtollers, views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,18 @@
 class TasksController < ApplicationController
   def new
+      @task = Task.new
   end
 
   def index
+      @tasks=Task.all
+  end
+  
+  def create
+      @task = Task.new(params[:task])
+      if @task.save
+          redirect_to tasks_path
+      else
+          render 'new'
+      end
   end
 end
