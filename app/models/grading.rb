5b4a4410c7b8fb5c93a0642d80141dcf26d1e9ae
Sneha Keshwani <snehak@wharton.upenn.edu>
2013-10-06 15:14:58 -0400

    fixed model for grading

--- app/models/grading.rb
+++ app/models/grading.rb
@@ -1,4 +1,4 @@
 class Grading < ActiveRecord::Base
   belongs_to :task
-  attr_accessible :percent, :importance, :finalgrade, :task
+  attr_accessible :percent, :importance, :finalgrade, :task_id
 end
