0909c94c19087c9d42a27a212e0277579b706fe9
Sneha Keshwani <snehak@wharton.upenn.edu>
2013-11-04 14:22:25 -0500

    sendgrid added

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,3 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '631713886879310', '7c16a1c36af660ce143726de198ef7ce'
 end
-
-OmniAuth.setup do |config|
-	config.mailer_sender = "tasks@example.com"
-end
